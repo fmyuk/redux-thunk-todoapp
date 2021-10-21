8c8030c43b70842544a1f030ae71cc5ac4c01ce4
fumi uk <dcfumiya.238@gmail.com>
2021-10-22 08:45:38 +0900

    todo

--- src/redux/reducers/todos.ts
+++ src/redux/reducers/todos.ts
@@ -1,11 +1,22 @@
+import { TodoActions } from "../actions";
+import { ActionTypes } from "../actionTypes";
 import { TodoState } from "../types";
 
 const initialState: TodoState = {
   todoItems: []
 };
 
-const todos = (state = initialState, action: any): any => {
-  return state;
-}
+const todos = (state = initialState, action: TodoActions): TodoState => {
+  switch (action.type) {
+    case ActionTypes.SET_TODOS: {
+      const { todos } = action.payload;
+      return {
+        todoItems: todos
+      };
+    }
+    default:
+      return state;
+  }
+};
 
 export default todos;
