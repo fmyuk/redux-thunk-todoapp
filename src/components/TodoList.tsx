ff379fc4756e285522f3be08749724cebeb71a2c
fumi uk <dcfumiya.238@gmail.com>
2021-10-25 08:45:02 +0900

    todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { setTodos, TodoActions } from "../redux/actions";
+import { RootState, TodoItem } from "../redux/types";
 import Todo from "./Todo";
 
-const TodoList = () => {
-  const todos: any = {};
+type TodoListProps = {
+  todos: Array<TodoItem>;
+  setTodos: (todos: Array<TodoItem>) => TodoActions;
+};
+
+const TodoList: React.FC<TodoListProps> = ({ todos, setTodos }) => {
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const response = await axios.get(`http://localhost:4000/todos`)
+        .catch((error) => {
+          throw new Error(error.message);
+        });
+      setTodos(response.data);
+    };
+    fetchTodos();
+  }, []);
+  
   return (
     <ul className="todo-list">
       {todos && todos.length
@@ -15,4 +34,9 @@ const TodoList = () => {
   );
 };
 
-export default TodoList;
+const mapStateToProps = (state: RootState) => {
+  const todos = state.todos.todoItems;
+  return { todos };
+};
+
+export default connect(mapStateToProps, { setTodos })(TodoList);
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { createStore, applyMiddleware } from "@reduxjs/toolkit";
+import thunk from 'redux-thunk';
 import rootReducer from "./reducers";
 
-const store = configureStore({
-  reducer: rootReducer
-});
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
 
 export default store;
