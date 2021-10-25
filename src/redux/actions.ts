b64767df836c268fea115f930b95aaae4daae88d
fumi uk <dcfumiya.238@gmail.com>
2021-10-26 08:50:45 +0900

    todo

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,5 +1,8 @@
+import axios from "axios";
+import { Dispatch } from "redux";
+import { ThunkAction } from "redux-thunk";
 import { ActionTypes } from "./actionTypes";
-import { TodoItem } from "./types";
+import { RootState, TodoItem } from "./types";
 
 type SetTodosAction = {
   type: ActionTypes.SET_TODOS;
@@ -10,5 +13,20 @@ export const setTodos = (todos: TodoItem[]): SetTodosAction => ({
   type: ActionTypes.SET_TODOS,
   payload: { todos }
 });
+
+export const fetchTodos = (): ThunkAction<
+  void,
+  RootState,
+  unknown,
+  TodoActions
+> => {
+  return async (dispatch: Dispatch<TodoActions>) => {
+    const response = await axios.get(`http://localhost:4000/todos`)
+      .catch((error) => {
+        throw new Error(error.message);
+      });
+    dispatch(setTodos(response.data));
+  };
+};
 
 export type TodoActions = SetTodosAction;
